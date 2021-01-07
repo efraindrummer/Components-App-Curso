f4a3db67433129d55f623212da3bc67c55b097b8
Efrain May <efrain_may_75@hotmail.com>
2021-01-07 16:54:46 -0600

    widget circle avatar - fadeInImage

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,8 +6,31 @@ class AvatarPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Avatar Page')
-      )
+        title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(2.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://avatars3.githubusercontent.com/u/58404444?s=460&u=1405a8e2db2674a9192f06c2b2be4c7d1d5a7221&v=4'),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://avatars3.githubusercontent.com/u/58404444?s=460&u=1405a8e2db2674a9192f06c2b2be4c7d1d5a7221&v=4'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        )
+      ),
     );
   }
 }
