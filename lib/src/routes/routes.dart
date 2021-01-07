5eb6584f30e85eadce7a61800a06c62131c6ba7c
Efrain May <efrain_may_75@hotmail.com>
2021-01-07 17:25:04 -0600

    container y animated container

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,4 +1,5 @@
 import 'package:componentes_flutter_app/src/pages/alert_page.dart';
+import 'package:componentes_flutter_app/src/pages/animated_container.dart';
 import 'package:componentes_flutter_app/src/pages/avatar_page.dart';
 import 'package:componentes_flutter_app/src/pages/card_page.dart';
 import 'package:componentes_flutter_app/src/pages/home_page.dart';
@@ -12,5 +13,6 @@ Map<String, WidgetBuilder> getAplicationRoutes(){
     'alert' : (BuildContext context) => AlertPage(),
     'avatar': (BuildContext context) => AvatarPage(),
     'card'  : (BuildContext context) => CardPage(),
+    'animatedContainer' : (BuildContext context) => AnimatedContainerPage(),
   };
 }
