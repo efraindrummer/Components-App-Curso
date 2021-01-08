3a62d58485d0686456e481d71175915dc3611fd1
Efrain May <efrain_may_75@hotmail.com>
2021-01-07 18:28:50 -0600

    input fields

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -3,6 +3,7 @@ import 'package:componentes_flutter_app/src/pages/animated_container.dart';
 import 'package:componentes_flutter_app/src/pages/avatar_page.dart';
 import 'package:componentes_flutter_app/src/pages/card_page.dart';
 import 'package:componentes_flutter_app/src/pages/home_page.dart';
+import 'package:componentes_flutter_app/src/pages/input_page.dart';
 
 import 'package:flutter/material.dart';
 
@@ -14,5 +15,6 @@ Map<String, WidgetBuilder> getAplicationRoutes(){
     'avatar': (BuildContext context) => AvatarPage(),
     'card'  : (BuildContext context) => CardPage(),
     'animatedContainer' : (BuildContext context) => AnimatedContainerPage(),
+    'inputs'            : (BuildContext context) => InputPage(),
   };
 }
