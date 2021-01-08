0b3e1dd2bf7692b96749a7ac4d9bd13d72a41ab6
Efrain May <efrain_may_75@hotmail.com>
2021-01-07 18:41:03 -0600

    Inputs de email y password fields

--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -7,7 +7,8 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
 
-  String _nombre;
+  String _nombre = '';
+  String _email = '';
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +20,10 @@ class _InputPageState extends State<InputPage> {
         padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
         children: <Widget>[
           _crearInput(),
+          Divider(),
+          _crearEmail(),
+          Divider(),
+          _crearPassword(),
           Divider(),
           _crearPersona(),
         ]
@@ -49,10 +54,48 @@ class _InputPageState extends State<InputPage> {
     );
   }
 
+  Widget _crearEmail(){
+    
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+        hintText: 'Email',
+        labelText: 'Email',
+        suffixIcon: Icon(Icons.alternate_email),
+        icon: Icon(Icons.alternate_email)
+      ),
+      onChanged: (valor) => setState(() {
+        _email = valor;
+      })
+    );
+  }
+
+  Widget _crearPassword(){
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+        hintText: 'Password',
+        labelText: 'Password',
+        suffixIcon: Icon(Icons.lock),
+        icon: Icon(Icons.lock)
+      ),
+      onChanged: (valor) => setState(() {
+        _email = valor;
+      })
+    );
+  }
+
   Widget _crearPersona(){
 
     return ListTile(
       title: Text('Nombre es: $_nombre'),
+      subtitle: Text('Email: $_email'),
     );
   }
 }
