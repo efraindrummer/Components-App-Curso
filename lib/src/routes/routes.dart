91a7ce1d838bb0ce3fc9eef1720c43fd94cc99b9
Efrain May <efrain_may_75@hotmail.com>
2021-01-08 01:04:55 -0600

    Sliders

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -4,6 +4,7 @@ import 'package:componentes_flutter_app/src/pages/avatar_page.dart';
 import 'package:componentes_flutter_app/src/pages/card_page.dart';
 import 'package:componentes_flutter_app/src/pages/home_page.dart';
 import 'package:componentes_flutter_app/src/pages/input_page.dart';
+import 'package:componentes_flutter_app/src/pages/slider_page.dart';
 
 import 'package:flutter/material.dart';
 
@@ -15,6 +16,7 @@ Map<String, WidgetBuilder> getAplicationRoutes(){
     'avatar': (BuildContext context) => AvatarPage(),
     'card'  : (BuildContext context) => CardPage(),
     'animatedContainer' : (BuildContext context) => AnimatedContainerPage(),
-    'inputs'            : (BuildContext context) => InputPage(),
+    'inputs': (BuildContext context) => InputPage(),
+    'slider':(BuildContext context) => SliderPage(),
   };
 }
